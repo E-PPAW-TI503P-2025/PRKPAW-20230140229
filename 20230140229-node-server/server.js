37d487d80355ab79f9574cf782e7a829717f1e1a
mahasiswa229 <mafaqih1945@gmail.com>
2025-10-02 07:52:23 +0700

    first commit

---
+++ 20230140229-node-server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, Muhammad Abdullah Faqih!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
