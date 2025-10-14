5461a9573864824f4943fd68890326272fb28870
mahasiswa229 <mafaqih1945@gmail.com>
2025-10-14 13:31:39 +0700

    Add desktop.ini for localized file names and create tugas2.md with API method images

--- 20230140229-node-server/server.js
+++ 20230140229-node-server/server.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+ 	const express = require('express');
+ 	const cors = require('cors'); 
+ 	const app = express();
+ 	const PORT = 3001;
+ 	
+ 	// Middleware
+ 	app.use(cors()); 
+ 	app.use(express.json()); 
+ 	app.use((req, res, next) => {
+ 	  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+ 	  next();
+ 	});
+ 	
+ 	app.get('/', (req, res) => {
+ 	  res.send('Home Page for API');
+ 	});
+ 	
+  const bookRoutes = require('./routes/books');
+ 	app.use('/api/books', bookRoutes);
 
-app.use(cors());
-const port = 5000;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello, Muhammad Abdullah Faqih!' });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+ 	app.listen(PORT, () => {
+ 	  console.log(`Express server running at http://localhost:${PORT}/`);
+ 	});
---
+++ tugas/tugas2.md
@@ -0,0 +1,8 @@
+# get
+![alt text](image-2.png)
+# post
+![alt text](image-3.png)
+# put
+![alt text](image-4.png)
+# delete
+![alt text](image-5.png)
